aeb5c283ad4aefdfb9c53a32f33cafc6e51b54c0
Feyzullah75 <fabdivaidov@gmail.com>
2020-03-24 17:54:06 -0500

    This is code update

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,10 @@
 public class Test {
+
+
     public static void main(String[] args){
 
         System.out.println("This is first commit");
+        System.out.println("This is code update");
 
 
     }
